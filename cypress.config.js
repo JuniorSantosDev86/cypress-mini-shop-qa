a418a1503b4ef4805df3ad955fbf2a02d2e041ed
Junior Santos <juniorsantos.dev86@gmail.com>
2026-04-09 15:22:36 -0300

    docs(report): add Mochawesome HTML test reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,22 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "Cypress Mini Shop Report",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     baseUrl: "http://localhost:3000",
     env: {
       email: "admin@test.com",
       password: "admin123"
     },
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
+    },
   },
 });
